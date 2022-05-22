ca021752d23c06cb3949be7981a9d1e5bff2dffa
naveen ijeri <naveen@caliperbusiness.com>
2022-05-22 15:38:59 +0530

    methods in oops

---
+++ cls_method.py
@@ -0,0 +1,5 @@
+#instance , class , static method
+
+#instance method - if we are using any instance variable then that method is called instance method
+#class method - if we are using any any static or class variable then that method is called class method
+#static method is called general utility methos in this methos we are not passing self, instance or static variable.
