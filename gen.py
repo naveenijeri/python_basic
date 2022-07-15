6fc6145ddfe90a7b19e4227e86833b2cc7fbd29a
naveen ijeri <43334898+naveenijeri@users.noreply.github.com>
2022-07-15 23:30:10 +0530

    Add files via upload

--- gen.py
+++ gen.py
@@ -1,12 +1,26 @@
-def simpleGeneratorFun():
-    yield 1            
-    yield 2            
-    yield 3            
+#refer: https://www.programiz.com/python-programming/generator
+#Iterators and Generators
+#Iterators: An iterator is an object that contains a countable number of values.
+#example: Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# def simpleGeneratorFun():
+#     yield 1            
+#     yield 2            
+#     yield 3            
    
-# Driver code to check above generator function
-for value in simpleGeneratorFun(): 
-    print(value)
+# # Driver code to check above generator function
+# for value in simpleGeneratorFun(): 
+#     print(value)
 
 
 #USAGE#####
 #it returns one element each time , it won't return all the elements so memory usage if efficient.
+
+a = ["apple", "orange", "banana"]
+print(dir(a))
